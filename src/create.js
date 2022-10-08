ea3bea3449a95a66b5d8598b1b16291c49e85248
raocz <1963196695@qq.com>
2022-10-08 15:07:18 +0800

    feat: 增加模版下载

--- src/create.js
+++ src/create.js
@@ -1,11 +1,60 @@
-import config from './config'
-import {downloadGitRepo} from './utils'
-import {prompt} from 'inquirer'
+const path = require("path");
+const fs = require("fs-extra");
+const chalk = require("chalk");
+const { prompt } = require("inquirer");
+const config = require("./config");
+const { downloadGitRepo } = require("./utils.js");
 
-export async function create(name,option){
-  const {template}= await prompt(config.templateList)
-  if(template)
-  {
-    downloadGitRepo(config.templateUrlMap[template],name)
+async function create(projectName, options) {
+  const { template } = await prompt(config.templateList);
+  const { force = false, merge = false } = options;
+  const cwd = options.cwd || process.cwd();
+  const inCurrent = projectName === ".";
+  const targetDir = path.resolve(cwd, projectName || ".");
+
+  if (fs.existsSync(targetDir) && !merge) {
+    if (force) {
+      await fs.remove(targetDir);
+    } else {
+      if (inCurrent) {
+        const { ok } = await prompt([
+          {
+            name: "ok",
+            type: "confirm",
+            message: `Generate project in current directory?`,
+          },
+        ]);
+        if (!ok) {
+          return;
+        }
+      } else {
+        const { action } = await prompt([
+          {
+            name: "action",
+            type: "list",
+            message: `Target directory ${chalk.cyan(
+              targetDir
+            )} already exists. Pick an action:`,
+            choices: [
+              { name: "Overwrite", value: "overwrite" },
+              { name: "Cancel", value: false },
+            ],
+          },
+        ]);
+        if (!action) {
+          return;
+        } else if (action === "overwrite") {
+          console.log(`\nRemoving ${chalk.cyan(targetDir)}...`);
+          await fs.remove(targetDir);
+        }
+      }
+    }
+  }
+  if (template) {
+    downloadGitRepo(config.templateUrlMap[template], targetDir);
   }
 }
+
+module.exports = {
+  create,
+};
